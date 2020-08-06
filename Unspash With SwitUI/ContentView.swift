07febf389db31684ea948c9dec7c2ffde9fccf69
vikesh <vikesh.prasad@mobiotics.com>
2020-08-06 21:57:38 +0530

    Initial Commit

---
+++ Unspash With SwitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unspash With SwitUI
+//
+//  Created by Vikesh Prasad on 06/08/20.
+//  Copyright © 2020 Mobiotics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
