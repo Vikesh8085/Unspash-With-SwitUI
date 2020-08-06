e525f4c2a4b9a9a2a8245f6ff2396094faf44248
vikesh <vikesh.prasad@mobiotics.com>
2020-08-06 22:28:24 +0530

    SDWebimage and git ignore File Added

--- Unspash With SwitUI/ContentView.swift
+++ Unspash With SwitUI/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HomeView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        VStack(){
+             Text("Hello, World!")
+        }
     }
 }
